4c50343fcf0998d6787d5a54968ae0b7aded27ab
Valerie Wee <sc19jrvw@leeds.ac.uk>
2024-12-22 22:58:14 +0000

    Improved day2

--- day02/main.py
+++ day02/main.py
@@ -9,124 +9,56 @@ def get_data():
 
     return data
 
-def solve_p1(data):
-    
-    increasing = True
-    decreasing = True
-    count = 0
-
-    for report in data:
-        for i in range(len(report)-1):
-            if report[i] < report[i+1] and report[i+1] - report[i] <= 3:
-                decreasing = False
-            elif report[i] > report[i+1] and report[i] - report[i+1] <= 3:
-                increasing = False
-            else:
-                increasing = False
-                decreasing = False
-        if increasing == True or decreasing == True:
-            count += 1
-        increasing = True
-        decreasing = True
-    return count
-
 def check_safe(report):
 
     increasing = True
     decreasing = True
 
     for i in range(len(report)-1):
-        if report[i] < report[i+1] and report[i+1] - report[i] <= 3:
+        diff = report[i + 1] - report[i]
+        if 1 <= diff <= 3:  # Valid increase
             decreasing = False
-        elif report[i] > report[i+1] and report[i] - report[i+1] <= 3:
+        elif -3 <= diff <= -1:  # Valid decrease
             increasing = False
-        else:
-            increasing = False
-            decreasing = False
-    if increasing == True or decreasing == True:
-        return True
+        else:  # Neither increasing nor decreasing
+            return False
 
+    return increasing or decreasing
+
+def dampener(report):
+
+    for i in range(len(report)):
+        if check_safe(report[:i] + report[i+1:]):
+            return True
+    
     return False
 
-def dampener(data):
+
+def p1(data):
 
     count = 0
 
     for report in data:
-        for i in range(len(report)):
-            if check_safe(report[:i] + report[i+1:]):
-                count += 1
-                break
-    
+        if check_safe(report):
+            count += 1
+
     return count
+
+def p2(data):
+
+    count = 0
+
+    for report in data:
+        if dampener(report):
+            count += 1
+
+    return count
+
             
 
 
 data = get_data()
-solve_p1(data)
-count = dampener(data)
-print(count)
-
-
-
-
-"""
-    increasing = 0
-    decreasing = 0
-    count = 0
-
-    for report in data:
-        for i in range(len(report)-1):
-            if report[i] < report[i+1] and report[i+1] - report[i] <= 3:
-                increasing += 1
-            elif report[i] > report[i+1] and report[i] - report[i+1] <= 3:
-                decreasing += 1
-            elif i+2 < len(report):
-                if report[i] < report[i+2] and report[i+2] - report[i] <= 3:
-                    increasing += 1
-                elif report[i] > report[i+2] and report[i] - report[i+2] <= 3:
-                    decreasing += 1
-        if (increasing+decreasing) >= len(report)-1:
-            count += 1
-        increasing = 0 
-        decreasing = 0
-    return count
-"""
-
-
-"""try 2
-count = 0
-
-    for report in data:
-
-        i=1
-        dampen = 0
-        increase = False
-        decrease = False
-
-        while i < len(report)-1:
-            
-            incA = report[i] - report[i-1]
-            incB = report[i+1] - report[i]
-            decA = report[i-1] - report[i]
-            decB = report[i] - report[i+1]
-
-            if 0<incA<=3 and 0<incB<=3:
-                increase = True
-                i += 1
-            elif 0<decA<=3 and 0<decB<=3:
-                decrease = True
-                i += 1
-            elif dampen == 0:
-                report.pop(i)
-                dampen += 1
-            else:
-                decrease = False
-                increase = False
-                break
-
-        if dampen <= 1 and increase == True and decrease == False:
-            count += 1 
-        elif dampen <=1 and increase == False and decrease == True:
-            count += 1
-"""
+p1 = p1(data)
+p2 = p2(data)
+print(p1)
+print(p2)
