abf6cb99bbcc4f92034f6ec524946e53d1822ef8
Valerie Wee <valwee77@gmail.com>
2024-12-23 21:30:22 +0000

    Complete d3p1

--- day03/main.py
+++ day03/main.py
@@ -3,39 +3,52 @@ def read_line():
     file_path = 'input.txt'
 
     with open(file_path, 'r') as file:
-        line = file.readline().strip()
+        line = file.read().replace('\n', '')
 
     return line
 
 def clean_line(line):
     begin = 0
     eqList = []
-    eq = ''
     while True:
         start = line.find('mul(', begin)
+        end = line.find(')', start)
 
         if start == -1:
             break
 
-        end = line.find(')', start)
-
-        eq = 'mul('
+        eq = ''
         for i in range(start + 4, end):
             char = line[i]
-            if not (char.isdigit() or char == ',' or char == '-'):
+            if not (char.isdigit() or char == ','):
                 eq = ''
                 break
             else:
                 eq += char
-                if i == end-1:
-                    eq += ')'
         
         if eq:
             eqList.append(eq)
 
         begin = start + 4
 
-    return 0
+    return eqList
+
+def multiply(x,y):
+    return x * y
+
+def compute(eqList):
+    
+    sum = 0
+
+    for eq in eqList:
+        x, y = map(int, eq.split(','))
+        sum += multiply(x,y)
+
+    return sum
+
+
 
 line = read_line()
 eqList = clean_line(line)
+sum = compute(eqList)
+print(sum)
