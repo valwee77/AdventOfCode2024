3ca497c3afa5ebb04d75ade087963a077b4ef9ad
Valerie Wee <valwee77@gmail.com>
2025-01-28 23:01:01 +0800

    Complete d1p1

--- day06/main.py
+++ day06/main.py
@@ -9,18 +9,6 @@ def read_data():
 
     return map
 
-def direction(guard):
-
-    if guard == "^":
-        print("up")
-        return 0,-1
-    elif guard == "v":
-        print("down")
-    elif guard == ">":
-        print("right")
-    else:
-        print("left")
-
 def find_guard(map):
 
     for i in range(len(map)):
@@ -29,23 +17,65 @@ def find_guard(map):
                 return (i,j)
     return None
 
+def direction(guard):
+
+    if guard == "^":
+        return -1,0
+    elif guard == ">":
+        return 0, 1
+    elif guard == "v":
+        return 1, 0
+    else:
+        return 0, -1
+
+def change_direction(guard):
+    if guard == "^":
+        return ">"
+    elif guard == ">":
+        return "v"
+    elif guard == "v":
+        return "<"
+    else:
+        return "^"
+
 def route(map):
     
-    i=0 #stores current position of guard
+    i,j = find_guard(map)
 
-    #find position of guard
     while i < len(map):
-        j=0
         while j < len(map[i]):
-            if map[i][j] not in ('.','#'): #finds position of guard
-                x,y = direction(map[i][j])
-                map[i+x][j+y] = map[i][j]
-                map[i][j] = "x"
-            j+=1
-        i+=1
+            x, y = direction(map[i][j]) #find next coordinates
+
+            new_i = i + x
+            new_j = j + y
+
+            if not (0 <= new_i < len(map) and 0 <= new_j < len(map[i])):
+                map[i][j] = "X"
+                return
+            if map[new_i][new_j] == "#":
+                guard = change_direction(map[i][j])
+                map[i][j] = guard
+                x, y = direction(map[i][j])
+                new_i = i + x
+                new_j = j + y
+
+            map[new_i][new_j] = map[i][j]
+            map[i][j] = "X"
+            i = new_i
+            j = new_j
+
+def count_positions(map):
+
+    count = 0
+
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == "X":
+                count += 1
                 
-
-
+    return count
 
 map = read_data()
 route(map)
+count = count_positions(map)
+print(count)
